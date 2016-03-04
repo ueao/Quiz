ee8a9bcf80777973a51295553070a8aa3cf97b59
Aoi Sakaue <aoiue.w.bbb@gmail.com>
2016-03-04 17:57:30 +0900

    完成！

--- Quiz/AppDelegate.swift
+++ Quiz/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let storyboard: UIStoryboard = StoryboardHelper.adjust()
+        window!.rootViewController = storyboard.instantiateInitialViewController()! as UIViewController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
